45f083769f4b7f16d36957d07c4166b3a628675c
d4g33z <jeff@du.local>
2025-08-19 20:32:17 -0400

    remove problematic import

--- pyncraft/logger.py
+++ pyncraft/logger.py
@@ -1,4 +1,14 @@
-import settings
+class Speed:
+
+    FASTEST=0.0005
+    FAST=0.001
+    MIDDLE=0.005
+    SLOW=0.01
+    SLOWEST=0.05
+
+SYS_SPEED=Speed.MIDDLE
+SHOW_DEBUG=True
+SHOW_Log=True
 
 class BColors:
     
@@ -29,5 +39,6 @@ def debug(*msg):
 def log(*msg):
     if(settings.SHOW_Log):
         print(BColors.LOG,msg,BColors.ENDC)
+
 def warn(*msg):
     print(BColors.WARNING,msg,BColors.ENDC)
